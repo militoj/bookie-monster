e99d1336ba7a871d09a922dd7815ce8ba99b8e56
militoj <militoj@gmail.com>
2017-04-20 04:34:12 -0500

    added apache commons dependency to build.gradle

--- src/main/java/org/launchcode/models/BookData.java
+++ src/main/java/org/launchcode/models/BookData.java
@@ -1,7 +1,19 @@
 package org.launchcode.models;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by johnmilito on 4/20/17.
