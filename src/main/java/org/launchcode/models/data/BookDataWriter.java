18c81daa482407bac877bcee70eefb45c848ec42
militoj <militoj@gmail.com>
2017-04-24 02:17:37 -0500

    created BookDataWriter and deleted the controllers as they are so full of errors due to my changes that they will likely need to be completey redone from scratch

---
+++ src/main/java/org/launchcode/models/data/BookDataWriter.java
@@ -0,0 +1,35 @@
+package org.launchcode.models.data;
+import org.launchcode.models.Book;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+/**
+ * Created by johnmilito on 4/24/17.
+ */
+//Borrowed from http://stackoverflow.com/questions/30073980/java-writing-strings-to-a-csv-file
+public class BookDataWriter {
+
+        public static void main(String[]args) throws FileNotFoundException{
+            PrintWriter pw = new PrintWriter(new File("test.csv"));
+            StringBuilder sb = new StringBuilder();
+            sb.append("id");
+            sb.append(',');
+            sb.append("Name");
+            sb.append('\n');
+
+
+            sb.append("1");
+            sb.append(',');
+            sb.append("Prashant Ghimire");
+            sb.append('\n');
+
+
+            pw.write(sb.toString());
+            pw.close();
+            System.out.println("done!");
+
+        }
+
+}
