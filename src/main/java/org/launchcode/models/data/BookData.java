0f6fe93eb9e53edcfb38357399c0bd631faca05a
militoj <militoj@gmail.com>
2017-04-24 01:03:21 -0500

    fixed or commented out all non-controller errors

--- src/main/java/org/launchcode/models/data/BookData.java
+++ src/main/java/org/launchcode/models/data/BookData.java
@@ -12,7 +12,7 @@ public class BookData {
     private ArrayList<Book> books = new ArrayList<>();
     private static BookData instance;
 
-    private BookFieldData<ISBN> employers = new BookFieldData<>();
+    private BookFieldData<ISBN> isbns = new BookFieldData<>();
     private BookFieldData<Quantity> locations = new BookFieldData<>();
     private BookFieldData<DateCreated> coreCompetencies = new BookFieldData<>();
     private BookFieldData<Price> positionTypes = new BookFieldData<>();
@@ -44,62 +44,62 @@ public class BookData {
     }
 
 
-    public ArrayList<Book> findByColumnAndValue(BookFieldType column, String value) {
+//    public ArrayList<Book> findByColumnAndValue(BookFieldType column, int value) {
+//
+//        ArrayList<Book> matchingBooks = new ArrayList<>();
+//
+//        for (Book book : books) {
+//            if (getFieldByType(book, column).contains(value))
+//                matchingBooks.add(book);
+//        }
+//
+//        return matchingBooks;
+//    }
 
-        ArrayList<Book> matchingBooks = new ArrayList<>();
 
-        for (Book book : books) {
-            if (getFieldByType(book, column).contains(value))
-                matchingBooks.add(book);
-        }
-
-        return matchingBooks;
-    }
-
-
-    public ArrayList<Book> findByValue(Float value) {
-
-        ArrayList<Book> matchingBooks = new ArrayList<>();
-
-        for (Book book : books) {
-
-            // TODO Figure out what this does where and fix it. why is name important?
-            if (book.getName().toLowerCase().contains(value)) {
-                matchingBooks.add(book);
-                continue;
-            }
-
-            for (BookFieldType column : BookFieldType.values()) {
-                if (column != BookFieldType.ALL && getFieldByType(book, column).contains(value)) {
-                    matchingBooks.add(book);
-                    break;
-                }
-            }
-        }
-
-        return matchingBooks;
-    }
+//    public ArrayList<Book> findByValue(Float value) {
+//
+//        ArrayList<Book> matchingBooks = new ArrayList<>();
+//
+//        for (Book book : books) {
+//
+//            // TODO Figure out what this does where and fix it. why is name important?
+//            if (book.getName().toLowerCase().contains(value)) {
+//                matchingBooks.add(book);
+//                continue;
+//            }
+//
+//            for (BookFieldType column : BookFieldType.values()) {
+//                if (column != BookFieldType.ALL && getFieldByType(book, column).contains(value)) {
+//                    matchingBooks.add(book);
+//                    break;
+//                }
+//            }
+//        }
+//
+//        return matchingBooks;
+//    }
 
 
     public void add(Book book) {
         books.add(book);
     }
 
-
-    private static BookField getFieldByType(Book book, BookFieldType type) {
-        switch(type) {
-            case ISBN:
-                return book.getEmployer();
-            case QUANTITY:
-                return book.getQuantity();
-            case PRICE:
-                return book.getDateCreated();
-            case DATE_CREATED:
-                return book.getPrice();
-        }
-
-        throw new IllegalArgumentException("Cannot get field of type " + type);
-    }
+//    TODO do I need this?
+//    private static BookField getFieldByType(Book book, BookFieldType type) {
+//        switch(type) {
+//            case ISBN:
+//                return book.getEmployer();
+//            case QUANTITY:
+//                return book.getQuantity();
+//            case PRICE:
+//                return book.getDateCreated();
+//            case DATE_CREATED:
+//                return book.getPrice();
+//        }
+//
+//        throw new IllegalArgumentException("Cannot get field of type " + type);
+//    }
 
     public BookFieldData<ISBN> getISBNs() {
         return isbns;
