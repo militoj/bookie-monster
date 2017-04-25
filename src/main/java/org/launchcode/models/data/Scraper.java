dfded08f9859a58de20fb645be2ff84773b7cb3d
militoj <militoj@gmail.com>
2017-04-24 21:29:46 -0500

    removed Jaunt files

--- src/main/java/org/launchcode/models/data/Scraper.java
+++ src/main/java/org/launchcode/models/data/Scraper.java
@@ -4,4 +4,12 @@ package org.launchcode.models.data;
  * Created by johnmilito on 4/24/17.
  */
 public class Scraper {
+    try{
+        UserAgent userAgent = new UserAgent();                       //create new userAgent (headless browser).
+        userAgent.visit("http://oracle.com");                        //visit a url
+        System.out.println(userAgent.doc.innerHTML());               //print the document as HTML
+    }
+catch(JauntException e){         //if an HTTP/connection error occurs, handle JauntException.
+        System.err.println(e);
+    }
 }
