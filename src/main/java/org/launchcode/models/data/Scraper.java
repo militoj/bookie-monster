efaa9e4a30bb6ea4b0c16c55b3c252895f72f49a
militoj <militoj@gmail.com>
2017-04-24 21:55:36 -0500

    figured out how to add jar file for Jaunt with Gradle. Scraper.java test is now functional

--- src/main/java/org/launchcode/models/data/Scraper.java
+++ src/main/java/org/launchcode/models/data/Scraper.java
@@ -1,15 +1,20 @@
 package org.launchcode.models.data;
 
+import com.jaunt.JauntException;
+import com.jaunt.UserAgent;
+
 /**
  * Created by johnmilito on 4/24/17.
  */
 public class Scraper {
-    try{
-        UserAgent userAgent = new UserAgent();                       //create new userAgent (headless browser).
-        userAgent.visit("http://oracle.com");                        //visit a url
-        System.out.println(userAgent.doc.innerHTML());               //print the document as HTML
-    }
-catch(JauntException e){         //if an HTTP/connection error occurs, handle JauntException.
-        System.err.println(e);
+
+    public static void main(String[]args) {
+        try {
+            UserAgent userAgent = new UserAgent();                       //create new userAgent (headless browser).
+            userAgent.visit("http://oracle.com");                    //visit a url
+            System.out.println(userAgent.doc.innerHTML());               //print the document as HTML
+        } catch (JauntException e) {                                     //if an HTTP/connection error occurs, handle JauntException.
+            System.err.println(e);
+        }
     }
 }
