f2fb3b1368dc9eade25fb9cd6da8331cb7c89044
militoj <militoj@gmail.com>
2017-04-24 23:33:03 -0500

    working with Scraper.java. I have been able to get it to print the entire HTML of the table using Element but haven't been able to print a row using getRow on a Table object

--- src/main/java/org/launchcode/models/data/Scraper.java
+++ src/main/java/org/launchcode/models/data/Scraper.java
@@ -1,18 +1,31 @@
 package org.launchcode.models.data;
 
-import com.jaunt.JauntException;
-import com.jaunt.UserAgent;
+import com.jaunt.*;
+import com.jaunt.component.*;
+import java.io.*;
+import java.util.*;
+
 
 /**
  * Created by johnmilito on 4/24/17.
  */
+// Learned how to use Jaunt at http://jaunt-api.com/jaunt-tutorial.htm
 public class Scraper {
 
     public static void main(String[]args) {
         try {
             UserAgent userAgent = new UserAgent();                       //create new userAgent (headless browser).
-            userAgent.visit("http://oracle.com");                    //visit a url
-            System.out.println(userAgent.doc.innerHTML());               //print the document as HTML
+            userAgent.visit("http://www.cash4books.net/recent500.php");                    //visit a url
+            Element table = userAgent.doc.findFirst("<table>");
+
+
+            System.out.println(table.getRow(5).outerHTML());
+
+//            System.out.println(table.outerHTML() + "\n----\n");
+
+
+
+
         } catch (JauntException e) {                                     //if an HTTP/connection error occurs, handle JauntException.
             System.err.println(e);
         }
